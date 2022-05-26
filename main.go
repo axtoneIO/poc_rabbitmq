c959b33f7aaad2d0f2e82b60380f929219dd4d28
Isela Ceballos Miranda <isela.ceballos@ikontechnologies.com>
2022-05-25 23:49:48 -0500

    RabbitMQ connection successfully implemented and working

--- main.go
+++ main.go
@@ -1,10 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/axtoneIO/poc_rabbitmq/internal/rabbitmq"
+)
 
+type App struct{
+	Rmq *rabbitmq.RabbitMQ
+}
 
 func Run() error{
 	fmt.Println("Go RabbitMQ POC")
+	rmq := rabbitmq.NewRabbitMQService()
+
+	app := App{
+		Rmq: rmq,
+	}
+
+	err := app.Rmq.Connect()
+	if err != nil {
+		return err
+	}
+	
 	return nil
 }
 
